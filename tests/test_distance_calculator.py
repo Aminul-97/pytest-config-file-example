f974a140c8f8f5ddd7d46f0563d155290a6c88b9
Aminul-97 <amin147570bd@gmail.com>
2024-01-08 03:16:25 +0600

    Config updated

--- tests/test_distance_calculator.py
+++ tests/test_distance_calculator.py
@@ -1,6 +1,8 @@
 from src.distance_calculator import kilometer_to_mile, mile_to_kilometer
 
 
-def test_distance_calculator():
+def test_kilometer_to_mile():
     assert kilometer_to_mile(5) == 3.106855
+
+def test_mile_to_kilometer():
     assert mile_to_kilometer(3.106855) == 5
